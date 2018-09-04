dd90d6336f6f0115e88ccf78eac90177a50464f3
Eder Marques <eder.marques@citi.org.br>
2018-09-04 20:35:25 -0300

    Improved bp

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+// ==================== EXTERNAL IMPORTS ==================== //
+
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// ==================== INTERNAL IMPORTS ==================== //
+
+// ==================== GLOBAL VARIABLES ==================== //
 
 const app = express();
+
+// ==================== MIDDLEWARE ==================== //
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -9,10 +19,32 @@ app.use((req, res, next) => {
     next();
 });
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// serving static files
+app.use('/views', express.static(path.join(__dirname, 'views')));
+
+// ==================== FUNCTIONS ==================== //
+
+// returns the full path of the passed view
+const getViewPath = view => path.join(__dirname, `views/${view}/${view}.html`);
+
+// ==================== ROUTES ==================== //
+
+// ==================== RENDER VIEWS ==================== //
+
 app.get('/', (req, res) => {
-    res.send('LISTENING ON PORT 3000');
+    res.sendFile(getViewPath('home'));
 });
+
+// ==================== START SERVER ==================== //
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('READY');
 });
+
+// ====================================================== //
