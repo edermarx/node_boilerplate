6f332886d555a4ddec396916ab6c7ddd2b8f532b
Eder Marques <edermarques96@gmail.com>
2019-10-21 07:59:01 -0300

    add eslint and fix some problems

--- providers/email-provider.js
+++ providers/email-provider.js
@@ -4,13 +4,14 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.MY_HOST,
   port: 465,
-  secure: true, // use SSL	
+  secure: true, // use SSL
   auth: {
     user: process.env.MY_EMAIL,
     pass: process.env.MY_PASSWORD,
   },
 });
 
+// eslint-disable-next-line object-curly-newline
 const sendEmail = ({ name, phone, email, message, subject }) => new Promise((resolve, reject) => {
   try {
     const mailOptions = {
